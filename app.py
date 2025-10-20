06365ae62caf46c3198e3d08a41f62fa298e985a
jade9890 <jadeluong789@gmail.com>
2025-10-20 17:47:18 +0100

    changed the line

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__) #creates an instance of Flask app
 
 @app.route("/") #route decorator, so / (root URL) is what triggers the function
 def home():
-	return "Hello, Welcome to lab 4! This is running on the local server " #home function returns a string
+	return "Hello, Welcome to Lab 4!" #home function returns a string
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0", port=5000) 
