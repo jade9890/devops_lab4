a50d272103bfe53afa71187441a1ecabe95a4b8f
jade9890 <jadeluong789@gmail.com>
2025-10-20 20:02:19 +0100

    it will now show fail bc of diff string

--- smoke_test.py
+++ smoke_test.py
@@ -1,37 +1,35 @@
-import unittest #pythons built in testing
-from app import app #imports the Flask app object I made from app.py
+import unittest #pythons own testing
 
-class TestAppSmoke(unittest.TestCase): #Define test class that inherits from unittest.TestCase
-	
-	def setUp(self): 
-		app.testing = True #puts Flask in testing mode
-		self.client = app.test_client() #create a fake browser
+from app import app  #import our web app
 
+ 
 
+class TestAppSmoke(unittest.TestCase): #create a class thats inherited from unittest
 
-#Test #1 Home route status
-#sends a GET request to / (homepage)
-#response.status_code gives the HTTP staatus (200 = OK, 404 = not found)
-#Asserts that response should be 200 (meaning it loaded successfully)
-#Basically it checks that the Flask route exists and works
+ 
 
-	def test_home_route(self):
-		response = self.client.get("/") 
-        	self.assertEqual(response.status_code, 200) 
+    def setUp(self):  #for the set up, puts app in testing most and makes a fake browse
 
+        app.testing = True 
 
-#test #2 check message content
-#Sends another GET request to /
-#response.data is the raw HTML returned by Flask
-#b"..." checks that "Hello, Welcome to Lab 4!" is acc inside the returned page
-#this makes sure my app returns the expected test content
+        self.client = app.test_client() 
 
-    	def test_message(self): 
-        	response = self.client.get("/") 
-        	self.assertIn(b"Hello, Welcome to Lab 4", response.data) 
+ 
 
- #below is saying, run the tests if this file is executed directly
-#When I run python test_app.py
+    def test_home_route(self): #get request for the homepage (/), asserts that response should be 200 (http status) meaning it loaded successfully (not 404)
+
+        response = self.client.get("/") 
+
+        self.assertEqual(response.status_code, 200) 
+
+    def test_message(self): #get request to / , compare bytes and checks the string is inside the returned page
+
+        response = self.client.get("/") 
+
+        self.assertIn(b"Hellooooooo, Welcome to Lab 4", response.data) 
+
+ 
 
 if __name__ == "__main__": 
-    unittest.main()
+
+    unittest.main() 
